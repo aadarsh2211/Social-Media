de12bffebd1a885b8b939990f95dfcd9641b26cf
aadarsh <aadarshchaurasia2220@gmail.com>
2023-04-28 17:53:49 +0530

    intilization

---
+++ app.js
@@ -0,0 +1,10 @@
+import Home from "./src/pages/home/Home";
+import Login from "./src/pages/login/Login";
+import Profile from "./src/pages/profile/Profile";
+import Register from "./src/pages/register/Register";
+
+function App() {
+  return <Profile/>;
+}
+
+export default App;
